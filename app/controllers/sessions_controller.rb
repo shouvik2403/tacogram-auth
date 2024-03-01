d70efb5d9238b17ea0a922b5ad047bfc1dd06e17
Shouvik Chaudhury <shouvik.chaudhury@kellogg.northwestern.edu>
2024-03-01 18:13:09 +0000

    commit 1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,25 @@ class SessionsController < ApplicationController
   
   def create
     # TODO: authenticate user
+    # First, try to find user using their unique identifier, i.e. email
+    @user = User.find_by({"email" => params["email"]})
+    if @user != nil
+      # if user exists, check if they know their password
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        # add a secure cookie for this user
+        session["user_id"] = @user["id"]  
+        # display welcome message
+        flash["notice"] = "Welcome #{@user["first_name"]}"
+        redirect_to "/posts"
+      else
+        flash["notice"] = "Sorry, wrong password!"
+        redirect_to "/login"
+      end  
+    else
+      flash["notice"] = "This email does not exist."
+      redirect_to "/login"    
+    end    
+  
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,23 @@ class UsersController < ApplicationController
     @user["first_name"] = params["first_name"]
     @user["last_name"] = params["last_name"]
     @user["email"] = params["email"]
-    # TODO: encrypt user's password "at rest"
     @user["password"] = params["password"]
-    @user.save
-    redirect_to "/posts"
+    
+    if @user["email"] == "" || @user["password"] == ""
+      flash["notice"] = "Email or Password cannot be blank!"
+      redirect_to "/users/new"
+    else
+      if User.find_by({"email" => @user["email"]}) == nil
+        # TODO: encrypt user's password "at rest"  
+        @user["password"] = BCrypt::Password.create(params["password"])
+        @user.save
+        redirect_to "/posts"
+      else   
+        flash["notice"] = "An account with this email already exists! Please try a different email or login to your account."
+        redirect_to "/users/new"
+      end
+    end  
+      
   end
+
 end
